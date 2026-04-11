36636af3cd7a7abdde1a45ecb99e8f40a03ea079
Zainabsaners <zainabsaners@gmail.com>
2026-04-12 00:24:17 +0300

    Add automated superuser creation for free tier

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# This line creates the admin automatically if it doesn't exist
+python manage.py createsuperuser --no-input || echo "Superuser already exists"
