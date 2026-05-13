e2be8731e3eb389f72d9383280801a0ed84ce362
Zainabsaners <zainabsaners@gmail.com>
2026-05-13 15:20:48 +0300

    Inspect users

--- backend/build.sh
+++ backend/build.sh
@@ -7,5 +7,21 @@ python manage.py migrate
 
 # This will print the live users to your Render Logs so you can see them!
 echo "--- STARTING LIVE USER LIST ---"
-python manage.py shell -c "from core.models import User; print([(u.username, u.is_superuser) for u in User.objects.all()])"
+python manage.py shell -c "
+from core.models import User;
+
+users = User.objects.all();
+
+print('===== USERS =====');
+
+for u in users:
+    print({
+        'username': u.username,
+        'email': u.email,
+        'is_superuser': u.is_superuser,
+        'is_staff': u.is_staff
+    });
+
+print('===== END USERS =====');
+"
 echo "--- ENDING LIVE USER LIST ---"
